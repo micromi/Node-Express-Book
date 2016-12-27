22cc39633f7928a4025f7c639fb91968f7ad4499
sunlight833 <484390775@qq.com>
2016-12-27 20:41:46 +0800

    ch05

---
+++ ch05/meadowlark.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var expresshbs = require('express-handlebars');
+var fortune = require('./lib/fortune.js');
+
+var app = express();
+
+//set up handlebars view engine
+var hbs = expresshbs.create({
+	defaultLayout: 'main',
+	layoutsDir: 'views/layouts',
+	extname: '.handlebars',
+  helpers: {
+    section: function (name, options){
+      if(!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    }
+  }
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+//设置端口
+app.set('port', process.env.PORT || 3000);
+//获取端口
+var port = app.get('port');
+//静态资源路径
+app.use(express.static(__dirname + '/public'));
+
+/*********** 设置路由 ************/
+// set 'showTests' context property if the querystring contains test=1
+app.use(function (req, res, next) {
+	res.locals.showTests = app.get('env') !== 'production' &&
+	  req.query.test === '1';
+	next();
+});
+app.get('', function (req, res) {
+	res.render('home');
+});
+
+app.get('/about', function (req, res) {
+	res.render('about', { 
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js'
+	});
+});
+
+app.get('/tours/hood-river', function (req, res){
+	res.render('tours/hood-river');
+});
+app.get('/tours/oregon-coast', function (req, res){
+	res.render('tours/oregon-coast');
+});
+app.get('/tours/request-group-rate', function (req, res){
+	res.render('tours/request-group-rate');
+});
+
+// 404 catch-all handler (middleware)
+app.use(function (req, res) {
+	res.status(404);
+	res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function (err, req, res, next) {
+	res.status(500);
+	res.render('500');
+});
+/*********** 设置路由end ************/
+
+app.listen(port, function () {
+	console.log('Epress started on http:localhost:' + port  + '; press Ctrl-C terminate...');
+});
